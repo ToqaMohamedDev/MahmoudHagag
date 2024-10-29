e36cb6c9eec6bcb39c4d73aebfdf6ff4e86d6df6
AlaaTaha <alaataha@192.168.1.9>
2024-10-29 04:47:28 -0700

    first 2

--- hooks/useScrollProgress.jsx
+++ hooks/useScrollProgress.jsx
@@ -27,7 +27,7 @@ export default useScrollprogress;
 echo "# MahmoudHagag" >> README.md
 git init
 git add README.md
-git commit -m "firs"
+git commit -m "first 1"
 git branch -M main
 git remote add origin https://github.com/ToqaMohamedDev/MahmoudHagag.git
 git push -u origin main
