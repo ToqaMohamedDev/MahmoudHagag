f51b656ef8504e9b1a2dd5a9ca6aed1afff5f529
AlaaTaha <alaataha@192.168.1.9>
2024-11-01 01:16:35 -0700

    first 15

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,10 @@
-'use client'
 import GsapAnimation from "@/components/GsapAnimation";
 import Hero from "@/components/Hero";
-import useBlobity from 'blobity/lib/react/useBlobity'
 import ScrollSection from "@/components/ScrollSection";
 import { Youtube } from "@/components/Youtube";
-import {initialBlobityOptions} from './utils/BlobityConfig'
-import { useEffect } from "react";
+
 
 export default function Home() {
-  const blobityInstanse=useBlobity(initialBlobityOptions);
-  useEffect(()=>{
- 
-  window.blobity=blobityInstanse.current;
-  },[blobityInstanse]);
   return (
 <div>
   <Hero/>
