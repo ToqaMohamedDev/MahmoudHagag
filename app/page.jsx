4bc26cfb29f1a8c242a4ab0195fc9125610ec360
AlaaTaha <alaataha@192.168.1.9>
2024-11-01 01:28:27 -0700

    first 16

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,21 @@
 import GsapAnimation from "@/components/GsapAnimation";
 import Hero from "@/components/Hero";
+import PreLoader from "@/components/PreLoader/PreLoader";
 import ScrollSection from "@/components/ScrollSection";
 import { Youtube } from "@/components/Youtube";
 
 
 export default function Home() {
   return (
-<div>
-  <Hero/>
-  <GsapAnimation/>
-  <Youtube/>
-  <ScrollSection/>
-</div>
+    <>
+      <PreLoader />
+      <div>
+        <Hero />
+        <GsapAnimation />
+        <Youtube />
+        <ScrollSection />
+      </div>
+    </>
   );
 }
 
